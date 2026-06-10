5345a08965fa9024e1c47b0ea17d20b9ac08815b
tomhubert50400 <73112644+tomhubert50400@users.noreply.github.com>
2026-06-11 07:50:39 +0900

    add Prisma database foundation

---
+++ apps/api/src/database/prisma.service.ts
@@ -0,0 +1,19 @@
+import { Inject, Injectable, OnModuleDestroy } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleDestroy {
+  constructor(@Inject(ConfigService) config: ConfigService) {
+    super({
+      adapter: new PrismaPg({
+        connectionString: config.getOrThrow<string>('DATABASE_URL'),
+      }),
+    });
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
